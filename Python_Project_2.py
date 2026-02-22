cbeef226451490b568f55acd1f68f73627e2fca7
Ritik Singh <singhritik3133@gmail.com>
2026-02-23 00:31:23 +0530

    Update Python_Project_2.py

--- Python_Project_2.py
+++ Python_Project_2.py
@@ -1,1 +1,14 @@
+# Python Project
+# Write a python program that builds a list of all files on your computer.
 
+import os
+
+path = "C:\\Users\\Ritik Singh"  #starting folder fath
+file_list = []
+
+for root, folders, files in os.walk(path):
+    for name in files:
+        full_name = root + "/" + name
+        file_list.append(full_name)
+
+print("Total number of files are:", len(file_list))
